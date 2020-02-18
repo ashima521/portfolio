0810646e9d615e93d28705e5e4b8208a2260ce67
ashima <ashima ghale>
2020-02-18 10:01:44 +0000

    Q2

--- Finding/src/com/qa/find/Find.java
+++ Finding/src/com/qa/find/Find.java
@@ -2,19 +2,15 @@ package com.qa.find;
 
 public class Find {
 	
-    public static void main (String[] args) {
-		String s = "hello";
-		
-		System.out.println(s.substring(1, 2));
-		if(s.indexOf('e') == 1) {
-			System.out.println("true");
-		}
-		else {
-			System.out.println("false");
-		}
-		
-	
+    	public boolean find(String sentence, int num, char character) {
+    	
+    	//return sentence.substring(num-1, num).equals(String.valueOf(character));
+    	
+   
+    	return sentence.charAt(num-1) == character;
+    	}
 	}
 
-}
+
+
 
---
+++ Finding/src/com/qa/find/Runner.java
@@ -0,0 +1,9 @@
+package com.qa.find;
+
+public class Runner {
+ 
+	public static void main(String[] args) {
+		Find character = new Find();
+		character.find("Hello", 2, 'e');
+	}
+}
