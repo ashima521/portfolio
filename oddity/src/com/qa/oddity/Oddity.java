2fcc88ff9a9cd647b38c9dd09a6b8954ea706802
ashima <ashima ghale>
2020-02-18 10:03:22 +0000

    Q3

--- oddity/src/com/qa/oddity/Oddity.java
+++ oddity/src/com/qa/oddity/Oddity.java
@@ -1,13 +1,27 @@
 package com.qa.oddity;
 
+import java.util.ArrayList;
+
 public class Oddity {
 	public static void main (String[] args) {
-		int num [] = {1,2,3,4,5,6};
+			
+		ArrayList<Integer> num = new ArrayList<Integer>();
+		num.add(1);
+		num.add(2);
+		num.add(3);
+		num.add(4);
 		
-		for(int i = 0; i < num.length; i++)  {
-			if(num[i] % 2 != 0) {
-				System.out.println(num[i]);
+		for(int nums: num) {
+			if(nums % 2 != 0) {
+				System.out.println(nums);
 			}
+			
+		
+//		for(int i = 0; i < num.length; i++)  {
+//		
+//			if(num[i] % 2 != 0) {
+//				System.out.println(num[i]);
+//			}
 		}
 	}
 
