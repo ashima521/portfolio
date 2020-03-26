830ecce37baf027625dcb552e75fe734dc4f3210
ashima <you@example.com>
2020-03-26 10:41:24 +0000

    Q10

---
+++ isogram/isogram/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace isogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Isogram is_Iso = new Isogram();
+            Console.WriteLine(is_Iso.IsItIsogram("hello"));
+        }
+        
+    }
+}
---
+++ isogram/isogram/isogram.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace isogram
+{
+    class Isogram
+    {
+        public bool IsItIsogram(string word)
+        {
+            string lw_word = word.ToLower();
+
+            char[] splitWord = lw_word.ToCharArray();
+            for (int i = 0; i < splitWord.Length; i++)
+            {
+                for (int j = i + 1; j < splitWord.Length; j++)
+                {
+                    if (splitWord[i] == splitWord[j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
